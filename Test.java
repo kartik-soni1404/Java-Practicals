899f0d5e5fc2607a934bf180180c6b5b888b057e
kartik-soni1707 <its.kartiksoni@gmail.com>
2018-04-19 20:07:31 +0530

    Example depicting abstract classes .

---
+++ Test.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Test {
+			public static  void main(String[] args) {
+			Child a = new Child();
+		    a.speak();
+			}
+			}
+abstract class Parent{
+			public Parent() {
+			//	Default constructor 
+			System.out.println("Inside test1");
+			}
+			//	An abstract method 	
+			public abstract void speak() ;
+			}
+class Child  extends Parent{
+			//Notice how the speak method is defined in the inherited class  
+public void speak() {
+			System.out.println("abstract method !!");
+			}	
+			}
+	
+	
