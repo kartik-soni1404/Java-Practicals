859c60fdbbdd84c36b61a0a1409ebebdf97405d7
kartik-soni1707 <its.kartiksoni@gmail.com>
2018-04-20 08:37:04 +0530

    Simple example of inheritance in Java

---
+++ Test.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class Test {
+			public static  void main(String[] args) {
+			Child a = new Child();
+		    a.speak();
+			a.dance();
+			//Now it will go to the parent class as it does not have a sing function in the Child class
+			a.sing();
+			}
+			}
+class  Parent{
+			//Default constructor 
+			public Parent() {
+			System.out.println("Inside the parent class ");
+			}
+			public void sing() {
+			System.out.println("Inside parent method ");
+			}
+			}
+class Child extends Parent{	
+public void speak() {
+			System.out.println("abstract method !!");
+			}	
+public void dance() {
+			System.out.println("abstract method no.2 !!");
+			}			
+			}
+	
+	
